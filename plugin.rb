cadb071b358e4af2c7ebaefe2557fb5ba018b255
NRT314 <newrussiatoken@proton.me>
2025-09-01 22:30:00 +0100

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -2,11 +2,12 @@
 # about: Adds "Login with Wallet" button (header + login modal fallback)
 # version: 0.1
 # authors: NewRussia
-# url: https://github.com/<your_org>/discourse-wallet-login
+# url: https://github.com/NRT314/discourse-wallet-login
 
 enabled_site_setting :wallet_login_enabled
 
 register_asset "stylesheets/common/wallet.scss"
+register_asset "javascripts/discourse/templates/connectors/login-after-login-buttons/wallet-login.hbs"
 
 after_initialize do
   # no server-side code required now
